62049a65c902ec808b9fe7dd6761cca896e3e750
Fire Phoenix <3301647@qq.com>
2024-12-03 19:50:35 +0800

    del then link

--- linkPublish.bat
+++ linkPublish.bat
@@ -1,11 +1,19 @@
 :: debug
 cd /d %~dp0publish\Debug
+del glm.lib
+del PhoenixMath.lib
+del CPUPathTracing.lib
 mklink glm.lib %~dp0build\glm\glm\glm.lib 
 mklink PhoenixMath.lib %~dp0..\PhoenixMath\build\Phoenix\Math\Debug\PhoenixMath.lib
-mklink CPUPathTracing.lib %~dp0..\CPUPathTracing\build\source\CPUPathTracing.lib
+mklink CPUPathTracing.lib %~dp0..\CPUPathTracing\build\source\Debug\CPUPathTracing.lib
 
 :: Release
 cd /d %~dp0publish\Release
+del glm.lib
+del PhoenixMath.lib
+del CPUPathTracing.lib
 mklink glm.lib %~dp0build\glm\glm\glm.lib  
 mklink PhoenixMath.lib %~dp0..\PhoenixMath\build\Phoenix\Math\Release\PhoenixMath.lib
-mklink CPUPathTracing.lib %~dp0..\CPUPathTracing\build\source\CPUPathTracing.lib
+mklink CPUPathTracing.lib %~dp0..\CPUPathTracing\build\source\Release\CPUPathTracing.lib
+
+cd /d %~dp0
